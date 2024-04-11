2f48a329e68b42564bc9ce3f12fc42ee1dad25ee
DeliceLydia <Delydia84@gmail.com>
2024-04-11 09:40:28 +0200

    Update connect config

--- db/connect.db.js
+++ db/connect.db.js
@@ -1,17 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log(`MongoDB connect :${connect.connection.host}`)
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.error(error);
+        process.exit(1);
     }
-}
+};
 
-
-module.exports = connectDB
+module.exports = connectDB;
